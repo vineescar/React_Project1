c03592c0b5b9edc8e9cb70d9fd52442a51111063
vineescar <vineescar21@gmail.com>
2023-07-09 16:12:18 +0530

    first commit

---
+++ src/PostLayout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+const PostLayout = () => {
+  return (
+    <>
+
+    <Link to="/postpage/1">Post 1</Link>
+        <br></br>
+        <Link to="/postpage/2">Post 2</Link>
+        <br></br>
+        <Link to="/postpage/3">Post 3</Link>
+        <br></br>
+        <Link to="/postpage/newpost">Newpost</Link>
+        <br></br>
+        <Outlet/>
+    
+    </>
+  )
+}
+
+export default PostLayout
